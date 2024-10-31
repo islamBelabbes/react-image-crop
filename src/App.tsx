da6473e292eb17d282a345441d4fa6c4e3607bda
islamBelabbes <belabbesislam2@gmail.com>
2024-10-31 20:46:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReactImageCrop from "./components/react-image-crop/react-image-crop";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center w-full min-h-screen text-white bg-black">
+      <ReactImageCrop />
+    </main>
+  );
+}
+
+export default App;
