d7ef27e6c4341b61797fca154f044463ba2db317
islamBelabbes <belabbesislam2@gmail.com>
2024-10-31 20:55:05 +0100

    fix styes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ReactImageCrop from "./components/react-image-crop/react-image-crop";
 
 function App() {
   return (
-    <main className="flex items-center justify-center w-full min-h-screen text-white bg-black">
+    <main className="flex justify-center w-full min-h-screen text-white bg-black py-28">
       <ReactImageCrop />
     </main>
   );
