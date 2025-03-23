59d67ac3bf3581b750c920c8bd891fe23ba25bed
islamBelabbes <belabbesislam2@gmail.com>
2025-03-23 14:31:20 +0100

    check for width change

--- src/hooks/use-window-size.ts
+++ src/hooks/use-window-size.ts
@@ -7,23 +7,26 @@ type TWindowSize = {
 
 export const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState<TWindowSize>({
-    width: 0,
-    height: 0,
+    width: window.innerWidth || 0,
+    height: window.innerHeight || 0,
   });
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+      // this check is needed because how the event works on mobile
+      if (windowSize.width !== window.innerWidth) {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
     };
 
     window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [windowSize]);
 
   return windowSize;
 };
